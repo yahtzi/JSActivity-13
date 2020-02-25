35c132e7ba6ffd933af57b62849812057736aeb2
Bejarano <yaritzabejarano@my.whccd.edu>
2020-02-24 21:13:38 -0800

    I've blocked my amazing link, what a shame

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let button = document.querySelector("button");
 
 function buttonClick() {
-  alert("Fool!");
+  alert("Wow, do you always do what the screen tells you? What a loser.");
   button.removeEventListener("click", buttonClick);
 }
 button.addEventListener("click", buttonClick);
+
+let link = document.querySelector("a");
+link.addEventListener("click", event => {
+    event.preventDefault();
+    alert("You're trapped stupid.");
+});
+
+
+ 
